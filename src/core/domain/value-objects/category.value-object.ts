e6f6157f8b9c0e8059ad8df51cdf40e2d0e112d5
Kitorosano <rosanoesteban2@gmail.com>
2025-05-02 13:07:03 -0300

    refactor: :fire: remove unused files

--- src/core/domain/value-objects/category.value-object.ts
+++
@@ -1,9 +0,0 @@
-type CategoryValue = 'food' | 'transport' | 'entertainment' | 'house' | 'other' | 'uncategorized';
-
-export class Category {
-  constructor(readonly value: CategoryValue) {}
-
-  equals(other: Category): boolean {
-    return this.value === other.value;
-  }
-}
--- src/core/domain/value-objects/currency.value-object.ts
+++
@@ -1,9 +0,0 @@
-type CurrencyValue = 'USD' | 'UYU' | 'COL';
-
-export class Currency {
-  constructor(readonly value: CurrencyValue) {}
-
-  equals(other: Currency): boolean {
-    return this.value === other.value;
-  }
-}
